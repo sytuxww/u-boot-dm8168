d544d41449626dfea145d105de54e164ccddc5f1
Administrator@XWW <sytu_xww@yahoo.com.cn>
2011-10-24 13:08:13 +0800

    debug gpio

--- common/cmd_gpio.c
+++ common/cmd_gpio.c
@@ -16,23 +16,24 @@
 int do_gpio (cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 {
 	  int gpio;
-    if (argc < 2)
+    if (argc < 3)
     	goto usage;
+    	
     gpio = simple_strtoul(argv[1], NULL , 10);
     printf("GPIO Number is %d!\n",gpio);
-    omap_request_gpio(gpio);
-    omap_set_gpio_direction(gpio,0);
+    //omap_request_gpio(gpio);
+    //omap_set_gpio_direction(gpio,0);
     if (strncmp(argv[2],"on",2) == 0)
     {
     	printf("GPIO %d on!\n",gpio);
-    	omap_set_gpio_dataout(gpio,1);
+    //	omap_set_gpio_dataout(gpio,1);
     }
     else if (strncmp(argv[2],"off",3) == 0)
     {
     	printf("GPIO %d off!\n",gpio);
-    	omap_set_gpio_dataout(gpio,0);
+    //	omap_set_gpio_dataout(gpio,0);
     }
-    omap_free_gpio(gpio);
+    //omap_free_gpio(gpio);
     return 0;
     
 usage:
@@ -41,7 +42,7 @@ usage:
 }
 
 U_BOOT_CMD(
- gpio,3,0,do_gpio,
+ gpio,3,1,do_gpio,
  "gpio - set or clear gpio in board!",
  " - set    gpio num on\n"
  " - clear  gpio num off\n"
