88d4e0ebed2b115f14072d1cedd4e1ce451df1c5
Administrator@XWW <sytu_xww@yahoo.com.cn>
2011-10-24 13:25:12 +0800

    debug gpio

--- common/cmd_gpio.c
+++ common/cmd_gpio.c
@@ -7,9 +7,21 @@
 #include <command.h>
 #include <asm/arch/gpio.h>
 
-
+void gpio_on_off(int gpio,char is_on)
+{
+	omap_request_gpio(gpio);
+	omap_set_gpio_direction(gpio,0);
+	
+	if(is_on)
+		omap_set_gpio_dataout(gpio,1); 
+	else
+		omap_set_gpio_dataout(gpio,0);
+	
+	omap_free_gpio(gpio);
+	
+}
 /*
- * do_menu
+ * do_gpio
  * 
  * 说明：uboot命令。
  */
@@ -21,22 +33,19 @@ int do_gpio (cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
     	
     gpio = simple_strtoul(argv[1], NULL , 10);
     printf("GPIO Number is %d!\n",gpio);
-    //omap_request_gpio(gpio);
-    //omap_set_gpio_direction(gpio,0);
+
     if (strncmp(argv[2],"on",2) == 0)
     {
     	printf("GPIO %d on!\n",gpio);
-    //	omap_set_gpio_dataout(gpio,1); 
+      gpio_on_off(gpio,1); 
     }
     else if (strncmp(argv[2],"off",3) == 0)
     {
     	printf("GPIO %d off!\n",gpio);
-    //	omap_set_gpio_dataout(gpio,0);
+      gpio_on_off,0);
     }
     else
     	goto usage;
-    	
-    //omap_free_gpio(gpio);
     return 0;
     
 usage:
@@ -47,8 +56,9 @@ usage:
 U_BOOT_CMD(
  gpio,3,1,do_gpio,
  "gpio - set or clear gpio in board!",
- " - set    gpio num on\n"
- " - clear  gpio num off\n"
+ "[num on|off] \n"
+ " 	- set    gpio num on\n"
+ " 	- clear  gpio num off\n"
 );
 
 
