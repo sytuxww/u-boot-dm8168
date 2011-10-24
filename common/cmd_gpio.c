203c907bf7cfa7db9591357baff52562d7d97ecd
Administrator@XWW <sytu_xww@yahoo.com.cn>
2011-10-24 13:00:00 +0800

    添加GPIO输出信息

--- common/cmd_gpio.c
+++ common/cmd_gpio.c
@@ -15,18 +15,21 @@
  */
 int do_gpio (cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 {
-	  char gpio;
+	  int gpio;
     if (argc < 2)
     	goto usage;
-    simple_strtoul(argv[1], &gpio, 16);
+    gpio = simple_strtoul(argv[1], NULL , 10);
+    printf("GPIO Number is %d!\n",gpio);
     omap_request_gpio(gpio);
     omap_set_gpio_direction(gpio,0);
     if (strncmp(argv[2],"on",2) == 0)
     {
+    	printf("GPIO %d on!\n",gpio);
     	omap_set_gpio_dataout(gpio,1);
     }
     else if (strncmp(argv[2],"off",3) == 0)
     {
+    	printf("GPIO %d off!\n",gpio);
     	omap_set_gpio_dataout(gpio,0);
     }
     omap_free_gpio(gpio);
